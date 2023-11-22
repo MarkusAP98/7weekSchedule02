a262f9203ddcbb1ce24f9b185aa1614551043bbe
Markus Phantha <markus.a.phantha@gmail.com>
2023-11-22 14:12:26 +0200

    Remove unnecessary CSS and update App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react'
 
 function App() {
 
   return (
     <>
-    
+    <h1>hey</h1>
     </>
   )
 }
